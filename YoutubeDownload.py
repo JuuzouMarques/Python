a247f454330d5a1a77662fd5bce155855a2e9a5b
JuuzouMarques <miltonneto2002@gmail.com>
2022-01-13 09:15:14 -0300

    Youtube Downloader
    Algoritmo para baixar vídeo do Youtube.com

---
+++ YoutubeDownload.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+
+print('-'*20)
+print('YOUTUBE DOWNLOADER')
+print('-'*20)
+link = input('Enter URL of video: ')
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+print('Downloading...')
+stream.download()
+print('\033[1;32mDownload Completed')
