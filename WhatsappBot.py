34135ac761b192c9504b2920adaf48df6423ad86
JuuzouMarques <miltonneto2002@gmail.com>
2022-01-31 12:04:35 -0300

    WhatsappBot
    Ajuste para saber quantas mensagens deseja enviar.

--- WhatsappBot.py
+++ WhatsappBot.py
@@ -1,9 +1,14 @@
 import pyautogui as auto
 from time import sleep
 
-print('Iniciando...')
+print('\033[1;32m-' * 20)
+print('WHATTSAPP BOT')
+print('-\033[m' * 20)
+msg = int(input('Digite a quantidade de mensagens você deseja enviar: '))
 sleep(10)
-while True:
+auto.write('T^o fazendo uns teste aqui. Vou te enviar mais {} mensagens'.format(msg))
+auto.press('enter')
+for c in range(msg):
     auto.write('Oi meu bem!')
     auto.press('enter')
     sleep(0.2)
