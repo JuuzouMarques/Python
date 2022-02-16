af276be76418d64fe6342ae134bbde42d87ebcc1
Juuzou Marques <miltonneto2002@gmail.com>
2022-02-16 14:33:50 -0300

    Versão 1

--- TestesTkinter.py
+++ TestesTkinter.py
@@ -1,41 +1,16 @@
-from ast import Slice
-from cProfile import label
-from fileinput import close
 from tkinter import *
 from random import randint
+import pyperclip as pc
+
+def verificaCampos():
+    if bMaiuscula.get() == False and bMinuscula.get() == False and bNumero.get() == False and bEspecial.get() == False:
+        senha['text'] = 'Selecione ao menos uma opção'
+        bttCopiar['state'] = DISABLED
+    else:
+        bttCopiar['state'] = NORMAL
+        gerarSenha()
 
 def gerarSenha():
-    sSenha = ""
-    sMin = 'abcdefghijklmnopqrstuvwxyz'
-    sMai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    sNum = '0123456789'
-    sEsp = '!@#$%&'
-    sFim = ''
-
-    nTamSenha = int(input('Digite o tamanho desejado para sua senha: '))
-    q = input('Deseja letras minúsculas em sua senha? [S/N] \n')
-    if q == 'S':
-        sSenha += sMin
-        q = ''
-    q = input('Deseja letras maiúsculas em sua senha? [S/N] \n')
-    if q == 'S':
-        sSenha += sMai
-        q = ''
-    q = input('Deseja números em sua senha? [S/N] \n')
-    if q == 'S':
-        sSenha += sNum
-        q = ''
-    q = input('Deseja símbolos em sua senha? [S/N] \n')
-    if q == 'S':
-        sSenha += sEsp
-        q = ''
-    for i in range(nTamSenha):
-        pos = randint(0, len(sSenha))
-        sFim += sSenha[pos]
-
-    print(f'Sua senha final é: {sFim}')
-
-def gerarSenha2():
     sSenha = ''
     sMin = 'abcdefghijklmnopqrstuvwxyz'
     sMai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -56,9 +31,10 @@ def gerarSenha2():
         pos = randint(0, (len(sSenha) - 1))
         sFinal += sSenha[pos]
     
-    senha['text'] = sFinal
-    print(bMaiuscula.get())
+    senha['text'] = (sFinal)
 
+def copiar():
+    pc.copy(senha['text'])
 
 janela = Tk()
 
@@ -71,21 +47,28 @@ nTamSenha = DoubleVar()
 janela.title('Gerador de Senhas')
 titulo = Label(janela, text='-'*30 + '\nGERADOR DE SENHAS\n' + '-'*30)
 titulo.grid(row=0, column=0, padx=20)
-optMaiuscula = Checkbutton(janela, text='Letras Maiúsculas', variable=bMaiuscula, onvalue=True, offvalue=False)
-optMinuscula = Checkbutton(janela, text='Letras Minúsculas', variable=bMinuscula, onvalue=True, offvalue=False)
-optNumeros = Checkbutton(janela, text='Números', variable=bNumero, onvalue=True, offvalue=False)
-optEspecial = Checkbutton(janela, text='Caracteres Especiais', variable=bEspecial, onvalue=True, offvalue=False)
 
-optMaiuscula.grid(row=1, column=0)
-optMinuscula.grid(row=2, column=0)
-optNumeros.grid(row=3, column=0)
-optEspecial.grid(row=4, column=0)
-button = Button(janela, text='Gerar Senha', command=gerarSenha2)
-button.grid(row=5, column=0)
-senha = Label(janela, text='')
-senha.grid(row=6, column=0)
-
+labelTamanhoSenha = Label(janela, text='Tamanho da Senha:')
+labelTamanhoSenha.grid()
+labelTamanhoSenha.grid(row=1, column=0)
 slider = Scale(janela, from_=1, to=30, orient=HORIZONTAL, variable=nTamSenha)
-slider.grid(row=7, column=0)
+slider.grid(row=2, column=0)
+optMaiuscula = Checkbutton(janela, text='Letras Maiúsculas', variable=bMaiuscula, onvalue=True, offvalue=False)
+optMaiuscula.grid(row=3, column=0)
+optMinuscula = Checkbutton(janela, text='Letras Minúsculas', variable=bMinuscula, onvalue=True, offvalue=False)
+optMinuscula.grid(row=4, column=0)
+optNumeros = Checkbutton(janela, text='Números', variable=bNumero, onvalue=True, offvalue=False)
+optNumeros.grid(row=5, column=0)
+optEspecial = Checkbutton(janela, text='Caracteres Especiais', variable=bEspecial, onvalue=True, offvalue=False)
+optEspecial.grid(row=6, column=0)
+
+button = Button(janela, text='Gerar Senha', command=verificaCampos)
+button.grid(row=7, column=0, pady=10)
+labelSenha = Label(janela, text='Senha Gerada:')
+labelSenha.grid(row=9, column=0)
+senha = Label(janela, text='')
+senha.grid(row=10, column=0)
+bttCopiar = Button(janela, text='Copiar Senha', command=copiar, state=DISABLED)
+bttCopiar.grid(row=11, column=0, pady=10)
 
 janela.mainloop()
