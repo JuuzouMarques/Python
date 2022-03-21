876315342fa7e98ad4112d0d73944b6130c47ffa
Juuzou Marques <miltonneto2002@gmail.com>
2022-03-21 13:10:22 -0300

    Fim do algoritmo que valida CPF

--- ValidarCPF.py
+++ ValidarCPF.py
@@ -6,19 +6,31 @@ def validateCPF(cpf):
     summation = 0
     multiplier = 10  
     for digit in firts9digits:
-        summation += digit * multiplier
+        summation += int(digit) * multiplier
         multiplier -= 1
     restDivision = summation % 11
-    if restDivision < 2:
-        firstCheckDigit = 0
-    else:
-        
+    if restDivision >= 2:
+        firstCheckDigit = 11 - restDivision
+
+    # Calculate Second Check Digit
+    firts10digits = firts9digits + str(firstCheckDigit)
+    summation = 0
+    multiplier = 11
+    for digit in firts10digits:
+        summation += int(digit) * multiplier
+        multiplier -= 1
+    restDivision = summation % 11
+    if restDivision >= 2:
+        secCheckDigit = 11 - restDivision
     
 
-
+    cpfVerified = firts9digits + str(firstCheckDigit) + str(secCheckDigit)
+    return True if cpf == cpfVerified else False
+    print('This CPF is VALID!' if cpf == cpfVerified else 'This CPF is INVALID!')
 
 if __name__ == '__main__':
     cpf = input('Enter a CPF for validation: ')
     cpf = cpf.strip().replace('.','').replace('-', '')
-    print(cpf)
+    print('This CPF is VALID!' if validateCPF(cpf) else 'This CPF is INVALID!')
+    
     
